37ced541bb0f32163301919892527b91b98f2050
OKdongge <dark2014301494@gmail.com>
2019-07-20 10:59:37 +0800

    change password

--- 邮件提醒.py
+++ 邮件提醒.py
@@ -1,35 +1,3 @@
-<<<<<<< HEAD
-"""import stmplib
-from email.mime.text import MIMEText
-from email.mime.multipart imort MIMEMultipart
-from emai.header import Header
-
-class send_email(self,title,path,name):
-    message = MIMEMultipart()#创建一个可以发送附件的的对象
-    message['Form'] = self.sender
-    message['To'] = ';'join(self,receivers)
-    message['Subject'] = Header(title,'utf-8')
-    message.attach(MIMEText(text,'plain','utf-8'))
-
-    att = MIMEText('第2834章 黑色警戒.txt','base64','utf-8')#注意一下
-    att['Content-Type'] = 
-"""
-from email.mime.text import MIMEText
-
-msg = MIMEText('hello.send by python...','plain','utf-8')
-from_addr = input('From:')
-password = input('Password:')
-
-to_addr = input('To:')
-smtp_server = input('SMTP server:')
-import smtplib
-
-server = smtplib.SMTP(smtp_server,25)
-server.set_debuglevel(1)
-server.login(from_addr,password)
-server.sendmail(from_addr,[to_addr],msg.as_string())
-server.quit()
-=======
 """import stmplib
 from email.mime.text import MIMEText
 from email.mime.multipart imort MIMEMultipart
@@ -84,4 +52,3 @@ for x in range(1,2):
     x += 1
 # 退出邮箱
 mailServer.quit()
->>>>>>> f9960611ea0e49d19fcf4e14f83dc2fce87e1f14
